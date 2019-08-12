b96591a4ee2fa90bc12908729b09bece53fe51c3
William Wong <wwong1987@gmail.com>
2019-08-11 23:12:05 -0700

    initial commit with conceptual notes

---
+++ drills.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* 
+- Trees are data structures that consists of nodes linked together in a certain way
+- Binary trees are trees with additional limits: each node can only have 0, 1, or 2 children (at most 2 children)
+- Binary search tree (BST) are trees where nodes in the left-hand branch of a node are GUARANTEED to have lower values than the node itself.
+
+*/
